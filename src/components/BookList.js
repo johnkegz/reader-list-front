7926d255e27ee81968db51513714e8425ed78d22
johnkegz <johnkal24@gmail.com>
2020-02-06 14:58:36 +0300

    Fetch items from server

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks';
+// import { graphql } from 'graphql';
 
+const getBooksQuery = gql`
+{
+    books{
+        name
+        genre
+        id
+    }
+}
+`
+
+function handleBooks(data){
+    const result = data.books.map(book =>{
+        console.log(book);
+        return (
+            <li key={book.id}>
+                {book.name}
+            </li>)
+    })
+    return result
+}
 function BookList() {
-    return (
-        <div>
-            Books
-        </div>
-    )
+    const { loading, error, data } = useQuery(getBooksQuery);
+    if (loading) return <p>Loading...</p>;
+    if (error) {
+        return <p>{error.message}</p>
+    }
+    return handleBooks(data)
 }
 
 export default BookList
